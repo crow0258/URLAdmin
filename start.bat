85ecca5af6242ab932669e54f6836031f83060d9
crow <491529736@qq.com>
2020-05-15 11:05:17 +0800

    fix:修复启动命令

--- start.bat
+++ start.bat
@@ -2,6 +2,6 @@
 @echo off
 pushd %~dp0
 powershell.exe -ExecutionPolicy Unrestricted -command  " ./urladmin.ps1 start"
-#powershell.exe -command  " ./urladmin.ps1 start"
+:: powershell.exe -command  " ./urladmin.ps1 start"
 popd
 pause
